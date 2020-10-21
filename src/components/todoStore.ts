7c7a8d65122b69cb79f33d140c83f6fef30492bd
RePloZ <herve.kay@ynov.com>
2020-10-21 13:01:31 +0200

    update package,json

--- src/components/todoStore.ts
+++ src/components/todoStore.ts
@@ -3,9 +3,25 @@ import { Todo } from "./interface";
 export default class TodoStore {
     public todos: Todo[] = []
     
-    addTodo (title: string): void {}
-    removeTodo (todo: Todo): void {}
-    toggle (todo:Todo, completed = true): void {}
-    editTodo (todo: Todo, title: string): void {}
-    cleanTodo (): void {}
+    addTodo (title: string): void {
+        this.todos = [{title, completed: false, _id: Math.floor(Math.random() * 1000000)}, ...this.todos]
+    }
+    removeTodo (todo: Todo): void {
+        this.todos = this.todos.filter(t => t !== todo);
+    }
+    toggle (todo:Todo, completed = true): void {
+        this.todos = this.todos.map(t => t === todo ? {...t, completed} : t);
+    }
+    
+    toggleAll (completed = true): void {
+        this.todos = this.todos.map(t => completed !== t.completed ? {...t, completed} : t);
+    }
+    
+    updateTitle (todo: Todo, title: string): void {
+        this.todos = this.todos.map(t => t === todo ? {...todo, title} : todo);
+    }
+    
+    cleanTodo (): void {
+        this.todos = this.todos.filter(t => !t.completed);
+    }
 }
